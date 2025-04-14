47d7bca87be9644c9b630c279b781b4c02843341
Benson12332622 <Benson46459937@gmail.com>
2025-04-14 23:07:21 +0800

    上傳網站資料

---
+++ index-cn/effects.js
@@ -0,0 +1,46 @@
+//h2和p的文字效果
+document.addEventListener("DOMContentLoaded", function() {
+    const h2TextElements = document.querySelectorAll('h2');
+	const pTextElements = document.querySelectorAll('p');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // 一旦顯示，停止觀察該元素
+            }
+        });
+    });
+
+    h2TextElements.forEach(element => {
+        observer.observe(element);
+    });
+	pTextElements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+//logo的淡出效果
+window.onload = function() {
+	setTimeout(function() {
+		FadeOutBlock();
+		document.getElementById('logo').classList.add('fade-out');
+		document.body.style.position = 'relative';
+		setTimeout(function() {
+			document.getElementById('logo').style.display = 'none'; // 隱藏遮罩層
+		}, 1000);
+	}, 2000); // 3000 毫秒（3秒）
+}
+
+function FadeOutBlock() {
+	const h2TextElement = document.getElementsByTagName('h2');
+	const pTextElement = document.getElementsByTagName('p');
+	
+	document.getElementsByClassName('video-container')[0].style.display = 'block';
+	for (var i = 0; i < h2TextElement.length; i++) {
+		h2TextElement[i].style.display = 'block';
+	}
+	for (var i = 0; i < pTextElement.length; i++) {
+		pTextElement[i].style.display = 'block';
+	}
+}
