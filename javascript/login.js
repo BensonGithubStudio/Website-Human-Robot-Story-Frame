d04475767691630a2d7252e38303b01880f0394d
LAPTOP-P64LT2U4\adt11 <bensonletterbox@gmail.com>
2026-06-07 15:18:32 +0800

    修改全新介面

---
+++ javascript/login.js
@@ -0,0 +1,59 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbx2CoWJLvL2Ywh8iu5hIRrc6s0rotoyiye9e2rbI66RJyX8kgwMgGPMficsg5iXbWiPXQ/exec";
+
+/* ======================================================
+   LOGIN
+====================================================== */
+
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", async (e)=>{
+
+    e.preventDefault();
+
+    const account = document.getElementById("account").value;
+    const password = document.getElementById("password").value;
+
+    if(!account || !password){
+        alert("請輸入帳號密碼");
+        return;
+    }
+
+    try{
+        const response =
+        await fetch(API_URL,{
+            method:"POST",
+            body:JSON.stringify({
+                action:"login",
+                account:account,
+                password:password
+            })
+        });
+
+        const result = await response.json();
+
+        console.log(result);
+
+        if(result.success){
+
+            /* 儲存登入資訊 */
+
+            localStorage.setItem(
+                "user",
+                JSON.stringify(result.user)
+            );
+
+            alert("登入成功");
+
+            window.location.href = "index.html";
+        }
+        else{
+            alert(result.message);
+        }
+
+    }
+    catch(error){
+        console.error(error);
+
+        alert("伺服器錯誤");
+    }
+});
